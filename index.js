6e51f3a26df4e5f37f1eef7c7338c4f691eeb39b
ivandgc <ivandgc@hotmail.com>
2017-08-10 09:54:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0;
+  function addBump() {counter++}
+  function getBumps() { return counter}
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  function inner(deadlyDevice) {return {animalType: animalType, deadlyDevice: deadlyDevice }}
+  return inner
+}
+
+let sharkCreator = createAnimal('shark')
+let sharkWithFrickinLaserbeam = createAnimal('Shark')('Laserbeam')
+let sharkWithFrickinCannon = createAnimal('Shark')('Cannon')
